ceae3f77f30bd774567fd749ed6a34c8a739e362
milica <milicagnjatovic18@gmail.com>
2021-11-30 20:28:23 +0100

    Implemented client functions

--- 07-GuessMyDrawing/client.cpp
+++ 07-GuessMyDrawing/client.cpp
@@ -1,3 +1,171 @@
 #include "client.h"
+#include <iostream>
 
+
+Client::Client(QString name, QObject *parent):
+  QObject(parent),
+  mName(name),
+  messageIn(new QTcpSocket(this)),
+  messageOut(new QTcpSocket(this))
+{
+  // connect and disconnect
+  connect(messageIn, &QTcpSocket::connected, this, &Client::connected);
+  connect(messageIn, &QTcpSocket::disconnected, this, &Client::disconnected);
+
+  // slot to take care of Reading messsages
+  connect(messageIn, &QTcpSocket::readyRead, this, &Client::onReadyRead);
+
+  // TODO error
+//   connect(messageIn, QOverload<QAbstractSocket::SocketError>::of(&QAbstractSocket::error), this, &Client::error);
+  connect(messageIn, SIGNAL(error(QAbstractSocket::SocketError)), this, SLOT(error(QAbstractSocket::SocketError)));
+}
+
+void Client::connectToServer(const QHostAddress &adress, quint16 port)
+{
+  messageIn->connectToHost(adress, port);
+  messageOut->connectToHost(adress, port);
+  // TODO obrada greske?
+  if (messageIn->state() != QAbstractSocket::ConnectedState || messageOut->state() != QAbstractSocket::ConnectedState)
+    std::cout << "NOT CONNECTED" << std::endl;
+}
+
+void Client::disconnectFromHost()
+{
+  messageIn->disconnectFromHost();
+  messageOut->disconnectFromHost();
+  std::cout << "disconected" <<std::endl;
+  // TODO obrada greske?
+}
+
+void Client::send(const QString &text)
+{
+  std::cout << "SENDING " << std::endl;
+  if (text.isEmpty())
+      return;
+
+//  QDataStream clientStream(messageOut);
+//  clientStream.setVersion(QDataStream::Qt_DefaultCompiledVersion);
+
+  QJsonObject message;
+  message["type"] = QString("message");
+  message["text"] = QString(text);
+  message["sender"] = QString(mName);
+//  clientStream << QJsonDocument(message).toJson(QJsonDocument::Compact);
+
+  messageIn->write(QJsonDocument(message).toJson(QJsonDocument::Compact));
+}
+
+void Client::onReadyRead()
+{
+  std::cout << "READING " << std::endl;
+
+  QByteArray jsonData;
+//  QDataStream socketStream(messageIn);
+//  socketStream.setVersion(QDataStream::Qt_DefaultCompiledVersion);
+
+  jsonData = messageIn->readAll();
+  std::cout << "Received: " <<jsonData.toStdString() << std::endl;
+
+  QJsonParseError parseError;
+  const QJsonDocument doc = QJsonDocument::fromJson(jsonData, &parseError);
+  if(parseError.error == QJsonParseError::NoError){
+      // data is valid json
+      if (doc.isObject()) // if it's json object we can recieve it
+        jsonRecieved(doc.object());
+      else
+        std::cout << "DOC IS NOT OBJECT" << std::endl;
+    }
+  else{
+    std::cout << "PARSING JSON ERR " << parseError.errorString().toStdString() << std::endl;
+  }
+  /*
+  for(;;){
+      // we start transaction so we can go back to previous state if error occurs
+      socketStream.startTransaction();
+
+       if(messageIn->state() == QAbstractSocket::ConnectedState)
+           std::cout << "CONECTED" << std::endl;
+       else
+         std::cout << "NOT CONECTED" << std::endl;
+
+
+      if(socketStream.status() == QDataStream::Ok)
+          std::cout << "OK" << std::endl;
+      else
+          std::cout << "NOT OK " << std::endl;
+
+//      socketStream >> jsonData;
+
+//      char* s = new char;
+//      std::cout << socketStream.readRawData(s, 200) << std::endl;
+//      std::cout << s << std::endl;
+
+      jsonData = messageIn->readAll();
+      std::cout << jsonData.toStdString() << std::endl;
+
+      break;
+//      std::cout << jsonData.toStdString() << std::endl;
+
+      if(socketStream.status() == QDataStream::Ok)
+          std::cout << "OK" << std::endl;
+      else
+          std::cout << "NOT OK " << std::endl;
+
+      if(socketStream.commitTransaction()){
+          // successfully read data, checking valid format
+          QJsonParseError parseError;
+          const QJsonDocument doc = QJsonDocument::fromJson(jsonData, &parseError);
+          if(parseError.error == QJsonParseError::NoError){
+              // data is valid json
+              if (doc.isObject()) // if it's json object we can recieve it
+                jsonRecieved(doc.object());
+            }
+          // after this loop will try to read more data
+        }
+      else {
+          std::cout << "Failed" << std::endl;
+          //read failed, socket goes to state it was before transaction started
+          break;
+        }
+
+    }
+    */
+}
+
+void Client::jsonRecieved(const QJsonObject &doc)
+{
+  const QJsonValue typeVal = doc.value(QLatin1String("type"));
+
+  if (typeVal.isNull() || !typeVal.isString()){
+      return; // empty or unknown message recieved
+    }
+
+  if(typeVal.toString().compare(QLatin1String("message"), Qt::CaseInsensitive) == 0){
+      const QJsonValue text = doc.value(QLatin1String("text"));
+      if (text.isNull() || !text.isString()){
+          return; // no text
+        }
+
+      const QJsonValue sender = doc.value(QLatin1String("sender"));
+      if (sender.isNull() || !sender.isString()){
+          return; // sender missing
+        }
+      emit messageReacieved(sender.toString(), text.toString());
+    }
+  else if(typeVal.toString().compare(QLatin1String("newuser"), Qt::CaseInsensitive) == 0){
+    const QJsonValue username = doc.value(QLatin1String("username"));
+    if (username.isNull() || !username.isString()){
+        return; // username missing or invalid
+      }
+    emit userJoined(username.toString());
+    }
+  else if(typeVal.toString().compare(QLatin1String("userdisconnected"), Qt::CaseInsensitive) == 0){
+    const QJsonValue username = doc.value(QLatin1String("username"));
+    if (username.isNull() || !username.isString()){
+        return; // username missing or invalid
+      }
+    emit userLeft(username.toString());
+    }
+
+}
 
--- 07-GuessMyDrawing/client.h
+++ 07-GuessMyDrawing/client.h
@@ -1,28 +1,52 @@
 #ifndef CLIENT_H
 #define CLIENT_H
 
-#include "QString"
-#include "QTcpSocket"
+#include <QString>
+#include <QTcpSocket>
+#include <QAbstractSocket>
+#include <QObject>
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QJsonParseError>
+#include <QByteArray>
 
-class Client
+#include "canvas.h"
+
+class Room; // forward declaration so we don't have circular dependency
+
+class Client : public QObject
 {
+  Q_OBJECT
+  Q_DISABLE_COPY(Client)
 public:
-  Client(QString name);
+  explicit Client(QString name, QObject *parent=nullptr);
 
   void connectToServer(const QHostAddress &adress, quint16 port);
   void disconnectFromHost();
   void send(const QString &text);
-  void recieve();
+
+private slots:
+  void onReadyRead();
+
+signals:
+  void connected();
+  void disconnected();
+  void messageReacieved(const QString &sender, const QString &test);
+  void userJoined(const QString &username);
+  void userLeft(const QString &username);
+  void error(QAbstractSocket::SocketError socketError);
 
 private:
-  QString name;
-//  Room* room;
+  QString mName;
+  Room* room;
   int points;
-//  Canvas* canvas
+  Canvas* canvas;
 
-  QTcpSocket messageIn;
-  QTcpSocket messageOut;
-  QTcpSocket canvasSocket;
+  QTcpSocket *messageIn;
+  QTcpSocket *messageOut;
+  QTcpSocket *canvasSocket;
+
+  void jsonRecieved(const QJsonObject &doc);
 };
 
 #endif // CLIENT_H
