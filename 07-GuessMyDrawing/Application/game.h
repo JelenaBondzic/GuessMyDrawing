28f44e6c7642e74e853e6ac01a4c12e16e1e5f12
JelenaBondzic <mi18131@alas.matf.bg.ac.rs>
2021-12-22 00:27:09 +0100

    Added ignoring x button

--- 07-GuessMyDrawing/Application/game.h
+++ 07-GuessMyDrawing/Application/game.h
@@ -28,7 +28,7 @@ public:
     int getDuration() const;
 
     void setDuration(int newDuration);
-    //void closeEvent( QCloseEvent* event );
+    void closeEvent( QCloseEvent* event ) override;
 
 private slots:
     void sendMessage();
@@ -36,6 +36,7 @@ private slots:
     void messageRecieved(const QString &sender, const QString &text);
     void userLeft(const QString &username);
     void userJoined(const QString &username);
+    void pbLeaveClicked();
 
 
 signals:
@@ -52,7 +53,7 @@ public slots:
 private slots:
 
     void You_Are_Host();
-    void on_Game_finished(int result);
+  //  void on_Game_finished(int result);
     void Start_Game();
     void Game_Over();
 private:
